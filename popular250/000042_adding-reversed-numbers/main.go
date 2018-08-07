7644a767714a15ff6103078a57fdedc57ad90f13
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-08-07 12:02:00 +0530

    Adds ID-42 ADDREV - Adding Reversed Numbers
     - adds solution for ID-42 ADDREV - Adding Reversed Numbers
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ popular250/000042_adding-reversed-numbers/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var testNum int
+	fmt.Scanf("%d", &testNum)
+
+	var results []int
+	reader := bufio.NewReader(os.Stdin)
+
+	for i := 0; i < testNum; i++ {
+		text, _ := reader.ReadString('\n')
+		text = strings.Replace(text, "\n", "", -1)
+		parts := strings.Split(text, " ")
+		if len(parts) != 2 {
+			continue
+		}
+
+		v1, _ := strconv.Atoi(parts[0])
+		v2, _ := strconv.Atoi(parts[1])
+		sum := reverseInt(v1) + reverseInt(v2)
+		sum = reverseInt(sum)
+
+		results = append(results, sum)
+	}
+
+	for _, v := range results {
+		fmt.Println(v)
+	}
+}
+
+func reverseInt(v int) int {
+	var rev int
+	for v > 0 {
+		digit := v % 10
+		rev = rev*10 + digit
+		v /= 10
+	}
+	return rev
+}
