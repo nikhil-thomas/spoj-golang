6db18a6f667f53f5c675a81b942aba0a85f36e98
Nikhil Thomas <nikhilthomas1@gmail.com>
2018-08-16 23:49:34 +0530

    Adds ID-5 PALIN - The Next Palindrome
     - adds solution using algorithm from geekforgeeks ID-5 PALIN - The Next Palindrome (not accepted yet)
       src: https://www.geeksforgeeks.org/given-a-number-find-next-smallest-palindrome-larger-than-this-number/
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

--- popular250/000005_the-next-palindrome/03_using-string-inputs/main.go
+++ popular250/000005_the-next-palindrome/03_using-string-inputs/main.go
@@ -28,30 +28,146 @@ import (
 	"fmt"
 	"math/big"
 	"os"
-	"strings"
+	"strconv"
 )
 
-func nextPalindrome(str string) string {
-	return ""
+type input struct {
+	n   []int
+	mid int64
+	len int64
+}
+
+func newInput(s string) *input {
+	nSlice := []int{}
+	for _, r := range s {
+		digit, _ := strconv.Atoi(string(r))
+		nSlice = append(nSlice, digit)
+	}
+
+	lenSlice := int64(len(nSlice))
+	mid := lenSlice / 2
+
+	if lenSlice%2 == 0 {
+		mid = mid - 1
+	}
+
+	inp := input{
+		n:   nSlice,
+		mid: mid,
+		len: lenSlice,
+	}
+
+	return &inp
+}
+
+func (inp *input) String() string {
+	str := ""
+	for _, d := range inp.n {
+		str += strconv.Itoa(d)
+	}
+	return str
+}
+
+func (inp *input) isAll9s() bool {
+	for _, d := range inp.n {
+		if d != 9 {
+			return false
+		}
+	}
+	return true
+}
+
+func (inp *input) convert9sToP() string {
+	str := ""
+	for i := int64(0); i < inp.len-1; i++ {
+		str += "0"
+	}
+	str = "1" + str + "1"
+	return str
+}
+
+func (inp *input) incMidAndMirrorLeft() string {
+	i := inp.mid
+	carry := 1
+	for i >= 0 {
+		inp.n[i] += carry
+		carry = inp.n[i] / 10
+		inp.n[i] = inp.n[i] % 10
+		i--
+	}
+	if carry != 0 {
+		inp.n = append([]int{carry}, inp.n...)
+		inp.n = append(inp.n, 0)
+	}
+
+	var j int64
+	if inp.len%2 == 1 {
+		i = inp.mid - 1
+		j = inp.mid + 1
+	} else {
+		i = inp.mid
+		j = inp.mid + 1
+	}
+	return inp.mirrorLeft(i, j)
+}
+
+func (inp *input) mirrorLeft(i, j int64) string {
+	for i >= 0 && j < inp.len {
+		inp.n[j] = inp.n[i]
+		i--
+		j++
+	}
+
+	return inp.String()
+}
+
+func (inp *input) nextPalindrome() string {
+	if inp.isAll9s() {
+		return inp.convert9sToP()
+	}
+
+	i := inp.mid
+	j := inp.mid + 1
+
+	if inp.len%2 == 1 {
+		i = i - 1
+	}
+	for i >= 0 && j < inp.len {
+		if inp.n[i] != inp.n[j] {
+			break
+		}
+		i--
+		j++
+	}
+	if i < 0 {
+		return inp.incMidAndMirrorLeft()
+	}
+
+	if inp.n[i] > inp.n[j] {
+		return inp.mirrorLeft(i, j)
+	}
+
+	return inp.incMidAndMirrorLeft()
+
 }
 
 func main() {
-	testCount := big.NewInt(0)
 	str := ""
 	fmt.Scanf("%s", &str)
-	testCount.SetString(str, 10)
 
-	inputs := []string{}
+	testCount, _ := big.NewInt(0).SetString(str, 10)
+
+	inputs := []*input{}
 
 	scanner := bufio.NewScanner(os.Stdin)
-	for i := big.NewInt(0); i.Cmp(testCount) != 0; i.Add(i, big.NewInt(int64(1))) {
+	for i := big.NewInt(0); i.Cmp(testCount) != 0; i.Add(i, big.NewInt(1)) {
 		scanner.Scan()
-		str = scanner.Text()
-		str = strings.Replace(str, "\n", "", -1)
-		inputs = append(inputs, str)
+		str := scanner.Text()
+
+		inputs = append(inputs, newInput(str))
 	}
 
 	for _, input := range inputs {
-		fmt.Println(nextPalindrome(input))
+		fmt.Println(input.nextPalindrome())
 	}
 }
